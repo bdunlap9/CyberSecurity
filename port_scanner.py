9d5f350e24ab36e1dfd1d97e5a768b116d95c884
Weeke <baileydunlap9@gmail.com>
2024-10-20 07:27:01 -0700

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -2,7 +2,7 @@ import logging, sys
 from scapy.all import *
 
 # Logging Scapy Errors
-logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+logging.getLogger('scapy.runtime').setLevel(logging.ERROR)
 
 class PortScanner:
     def __init__(self, target, startport, endport):
@@ -28,23 +28,23 @@ class PortScanner:
                     self.open_ports.append(port)
                     rst_pkt = IP(dst=self.target) / TCP(dport=port, flags='R')
                     send(rst_pkt, verbose=0)
-                    print(f"Port {port}: Open")
+                    print(f'Port {port}: Open')
                 elif resp[TCP].flags == 0x14:  # RST
-                    # print(f"Port {port}: Closed")
+                    # print(f'Port {port}: Closed')
                     continue
 
     def report_open_ports(self):
         if self.open_ports:
-            print(f"\nOpen ports: {', '.join(map(str, self.open_ports))}")
+            print(f'\nOpen ports: {", ".join(map(str, self.open_ports))}')
         else:
-            print("No open ports found.")
+            print('No open ports found.')
 
 def main():
     print('''Port Scanner v1.0''')
 
     if len(sys.argv) != 4:  # port_scanner.py 127.0.0.1 1 65535
         print('Invalid arguments given!')
-        print("Usage: %s target startport endport" % (sys.argv[0]))
+        print('Usage: %s target startport endport' % (sys.argv[0]))
         sys.exit(1)
 
     target = str(sys.argv[1])
@@ -58,5 +58,5 @@ def main():
     scanner.scan_ports()
     scanner.report_open_ports()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
